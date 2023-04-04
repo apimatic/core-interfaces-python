eb50835681ae8ebb79d7b485d9e441d379d22df4
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2023-04-04 13:07:32 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='apimatic-core-interfaces',
-    version='0.1.2',
+    version='0.1.3',
     description='An abstract layer of the functionalities provided by apimatic-core-library, requests-client-adapter '
                 'and APIMatic SDKs.',
     long_description=long_description,
