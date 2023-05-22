0c58e71ff45425855f03ef5f5dcfcb4863075108
Muhammad Sufyan <sufyankhanrao123@gmail.com>
2023-05-22 16:12:01 +0500

    adds getter for context in union types

--- apimatic_core_interfaces/types/union_type.py
+++ apimatic_core_interfaces/types/union_type.py
@@ -1,11 +1,10 @@
 from abc import ABC, abstractmethod
-from apimatic_core.types.union_types.union_type_context import UnionTypeContext
 
 
 class UnionType(ABC):
     NATIVE_TYPES = [int, str, float, bool]
 
-    def __init__(self, union_types, union_type_context: UnionTypeContext):
+    def __init__(self, union_types, union_type_context):
         self._union_types = union_types
         self._union_type_context = union_type_context
         self.is_valid = False
@@ -17,3 +16,6 @@ class UnionType(ABC):
     @abstractmethod
     def deserialize(self, value):
         ...
+
+    def get_context(self):
+        return self._union_type_context
