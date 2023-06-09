609e253feb542e5220da59f8fbe79a28af5f480d
Muhammad Sufyan <sufyankhanrao123@gmail.com>
2023-06-09 12:21:35 +0500

    removed error message since these are kept in core library project

--- apimatic_core_interfaces/types/union_type.py
+++ apimatic_core_interfaces/types/union_type.py
@@ -3,8 +3,6 @@ from abc import ABC, abstractmethod
 
 class UnionType(ABC):
     NATIVE_TYPES = [int, str, float, bool]
-    NONE_MATCHED_ERROR_MESSAGE = 'We could not match any acceptable types against the given JSON.'
-    MORE_THAN_1_MATCHED_ERROR_MESSAGE = 'There are more than one acceptable type matched against the given JSON.'
 
     def __init__(self, union_types, union_type_context):
         self._union_types = union_types
