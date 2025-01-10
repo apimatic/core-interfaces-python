59b78b64e1e60d1913d0224f4e356bfe173d3a1c
DESKTOP-10GD4VQ\Sufyan <muhammad.sufyan@apimatic.io>
2025-01-10 18:42:44 +0500

    updates the type of the body of the response

--- apimatic_core_interfaces/factories/response_factory.py
+++ apimatic_core_interfaces/factories/response_factory.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from typing import Dict
+from typing import Dict, Union, Any
 
 from apimatic_core_interfaces.http.http_request import HttpRequest
 
@@ -8,5 +8,5 @@ from apimatic_core_interfaces.http.http_request import HttpRequest
 class ResponseFactory(ABC):
 
     @abstractmethod
-    def create(self, status_code: int, reason: str, headers: Dict[str, str], body: str, request: HttpRequest):
+    def create(self, status_code: int, reason: str, headers: Dict[str, str], body: Union[str, bytes], request: HttpRequest):
         ...
